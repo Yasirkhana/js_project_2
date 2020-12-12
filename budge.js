2126deba7748501429ac93019fc7f1bd80ba9b53
Yasir Ahmad <khan10055yasir@gmail.com>
2020-12-12 21:06:10 +0500

    budge app
    adding budge app

---
+++ budge.js
@@ -0,0 +1,12 @@
+budge_input_box = document.getElementById("budge-input");
+expense_input_box = document.getElementById("budge-input");
+expenseAmount_input_box = document.getElementById("budge-input");
+budgeAmount_label=document.getElementById("budgeAmount");
+expenseAmount_label=document.getElementById("expenseAmount");
+balanceAmount_label=document.getElementById("balanceAmount");
+
+
+
+
+
+
